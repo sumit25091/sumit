d0b56d15172bbe3af900867df5aac9df51f71635
sumit25091 <sumittolmatti@gmail.com>
2024-06-23 20:51:38 +0530

    Update untitle8.cpp

--- untitle8.cpp
+++ untitle8.cpp
@@ -5,14 +5,11 @@ int main()
 
     float side_length, area;
 
-    // Input side length of the square
     printf("Enter the side length of the square: ");
     scanf("%f", &side_length);
 
-    // Calculate the area of the square
     area = side_length * side_length;
 
-    // Print the area
     printf("The area of the square with side length %.2f is %.2f\n", side_length, area);
 
     return 0;
