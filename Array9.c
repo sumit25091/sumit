819f141bf4b1abee1287e3b721b70fd3a1e269a5
sumit25091 <sumittolmatti@gmail.com>
2024-07-09 20:38:21 +0530

    Array sorting

---
+++ Array9.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void main()
+{
+	int x[5], i, j, t;
+	
+	printf("\n Enter 5 integer numbers :");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&x[i]); //89 13 57 91 34
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		for(j=i+1;j<5;j++)
+		{
+			if(x[i]>x[j])
+			{
+				t = x[i];
+				x[i] = x[j];
+				x[j] = t;
+			}
+		}
+	}
+	
+	printf("\n array values in ascending order");
+	for(i=0;i<5;i++)
+	{
+		printf(" %d",x[i]);
+	}
+}
+
